02ec30f3f02fd0654967132adfe284b0041240e3
psycho <59530191+psychologyz@users.noreply.github.com>
2022-03-27 23:32:42 +0000

    Add files via upload

--- auth/register.tsx
+++ auth/register.tsx
@@ -1,10 +1,11 @@
 import axios from 'axios'
 
-export default async function register(email: string, password: string, invite: string) {
+export default async function register(email: string, password: string, invite: string, domain: string) {
     const res = await axios.post('https://api.radiant.cool/register', {
         email,
         password,
-        invite
+        invite,
+
     })
     return res.data
 }
