77893985b4649991b612d5be98301ea367c9f182
jaculate <98663009+jaculate@users.noreply.github.com>
2022-03-31 09:03:47 -0400

    fix: use correct endpoint for register

--- auth/register.tsx
+++ auth/register.tsx
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
 export default async function register(email: string, password: string, invite: string, domain: string) {
-    const res = await axios.post('https://api.radiant.cool/register', {
+    const res = await axios.post('https://api.radiant.cool/auth/register', {
         email,
         password,
         invite,
-
+        domain
     })
     return res.data
 }
